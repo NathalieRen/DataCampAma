fb84f361131bd5904c67f338cb87dc976949dfea
NathalieRen <nathalieren@gmail.com>
2015-07-04 11:51:46 +0200

    Tentative 2

--- feature_extractor.py
+++ feature_extractor.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 04 11:44:12 2015
+
+@author: NR705624
+"""
+import numpy as np
 import pandas as pd
 import os
 
@@ -10,19 +17,37 @@ class FeatureExtractor(object):
 
     def transform(self, X_df):
         X_encoded = X_df
-        path = os.path.dirname(__file__)
-        data_weather = pd.read_csv(os.path.join(path, "data_weather.csv"))
-        X_weather = data_weather[['Date', 'AirPort', 'Max TemperatureC']]
-        X_weather = X_weather.rename(columns={'Date': 'DateOfDeparture', 'AirPort': 'Arrival'})
-        X_encoded = X_encoded.set_index(['DateOfDeparture', 'Arrival'])
-        X_weather = X_weather.set_index(['DateOfDeparture', 'Arrival'])
-        X_encoded = X_encoded.join(X_weather).reset_index()
         
-        X_encoded = X_encoded.join(pd.get_dummies(X_encoded['Departure'], prefix='d'))
-        X_encoded = X_encoded.join(pd.get_dummies(X_encoded['Arrival'], prefix='a'))
-        X_encoded = X_encoded.drop('Departure', axis=1)
-        X_encoded = X_encoded.drop('Arrival', axis=1)
+        #data_oil = pd.read_csv("./DataLake/data_oil.csv")
+        data_oil = pd.read_csv(os.path.join(path, "data_oil.csv"))
+        #data_oil = data_oil.rename(columns={'Date': 'DateOfDeparture'})
+        
+        #data = pd.merge(X_df, data_oil, on='DateOfDeparture')     
+        data = X_encoded.merge(data_oil, how='left', left_on=['DateOfDeparture'], right_on=['Date'], sort=False)       
+        data = data.drop('Date', axis=1)
+        
+        data_encoded = data
 
-        X_encoded = X_encoded.drop('DateOfDeparture', axis=1)
-        X_array = X_encoded.values
+        data_encoded = data_encoded.join(pd.get_dummies(data_encoded['Departure'], prefix='d'))
+        data_encoded = data_encoded.join(pd.get_dummies(data_encoded['Arrival'], prefix='a'))
+        data_encoded = data_encoded.drop('Departure', axis=1)
+        data_encoded = data_encoded.drop('Arrival', axis=1)
+
+        data_encoded['DateOfDeparture'] = pd.to_datetime(data_encoded['DateOfDeparture'])
+        data_encoded['year'] = data_encoded['DateOfDeparture'].dt.year
+        data_encoded['month'] = data_encoded['DateOfDeparture'].dt.month
+        data_encoded['day'] = data_encoded['DateOfDeparture'].dt.day
+        data_encoded['weekday'] = data_encoded['DateOfDeparture'].dt.weekday
+        data_encoded['week'] = data_encoded['DateOfDeparture'].dt.week
+        data_encoded['n_days'] = data_encoded['DateOfDeparture'].apply(lambda date: (date - pd.to_datetime("1970-01-01")).days)
+
+        data_encoded = data_encoded.join(pd.get_dummies(data_encoded['year'], prefix='y'))
+        data_encoded = data_encoded.join(pd.get_dummies(data_encoded['month'], prefix='m'))
+        data_encoded = data_encoded.join(pd.get_dummies(data_encoded['day'], prefix='d'))
+        data_encoded = data_encoded.join(pd.get_dummies(data_encoded['weekday'], prefix='wd'))
+        data_encoded = data_encoded.join(pd.get_dummies(data_encoded['week'], prefix='w'))
+
+        features = data_encoded.drop(['DateOfDeparture'], axis=1)
+        X_array = features.values
+        
         return X_array
--- regressor.py
+++ regressor.py
@@ -1,9 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 04 11:47:54 2015
+
+@author: NR705624
+"""
+
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.base import BaseEstimator
 
 class Regressor(BaseEstimator):
     def __init__(self):
-        self.clf = RandomForestRegressor(n_estimators=10, max_depth=10, max_features=10)
+        self.clf = RandomForestRegressor(n_estimators=316, max_depth=50, max_features=10)
 
     def fit(self, X, y):
         self.clf.fit(X, y)
